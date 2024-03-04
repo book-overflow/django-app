4593506b8691b24e1aebe4024069c1083444679b
timothycao <tcao.pro@gmail.com>
2024-03-03 22:22:50 -0500

    CustomUserCreationForm, CustomUserChangeForm, and basic urls/views/templates for user register, login, logout, profile

--- src/django/app/bookoverflow/urls.py
+++ src/django/app/bookoverflow/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from user import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='user-register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='user-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='user-logout'),
+    path('user/', include('user.urls')),
     path('helloworld/', include('helloworld.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
--- src/django/app/user/views.py
+++ src/django/app/user/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user-login')
+    else:
+        form = CustomUserCreationForm()
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+def profile(request):
+    return render(request, 'profile.html')
