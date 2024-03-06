9dd1d879507cd2c891ac36d721d5511d604e5344
Clarke Patrone <clarkepatrone@gmail.com>
2024-03-05 23:32:43 -0500

    Test cases for Custom User Models

--- src/django/app/user/tests.py
+++ src/django/app/user/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class CustomUserManagerTests(TestCase):
+    def test_create_user(self):
+        manager = get_user_model().objects
+        email = "test@example.com"
+        password = "testpassword"
+        user = manager.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+        self.assertFalse(user.is_staff)
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        manager = get_user_model().objects
+        email = "admin@example.com"
+        password = "adminpassword"
+        superuser = manager.create_superuser(email=email, password=password)
+
+        self.assertEqual(superuser.email, email)
+        self.assertTrue(superuser.check_password(password))
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_active)
+        self.assertTrue(superuser.is_superuser)
+
+
+class CustomUserModelTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            email="test@example.com", password="testpassword"
+        )
+
+        self.assertEqual(user.email, "test@example.com")
+        self.assertFalse(user.is_staff)
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        superuser = User.objects.create_superuser(
+            email="admin@example.com", password="adminpassword"
+        )
+
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_active)
+        self.assertTrue(superuser.is_superuser)
