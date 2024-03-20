06ad1543a87d29fd6e48c60542dc02725d1de72b
timothycao <tcao.pro@gmail.com>
2024-03-20 15:50:27 -0400

    Added guest_required and profile_required decorators to views

--- src/django/app/user/views.py
+++ src/django/app/user/views.py
@@ -1,7 +1,9 @@
+from .forms import CustomUserCreationForm, UserProfileForm
+from .decorators import guest_required, profile_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm, UserProfileForm
 
+@guest_required
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
@@ -15,6 +17,7 @@ def register(request):
     return render(request, 'register.html', context)
 
 @login_required
+@profile_required
 def profile(request):
     return render(request, 'profile.html')
 
@@ -22,6 +25,7 @@ def setProfile(request):
     return render(request, 'setProfile.html')
 
 @login_required
+@profile_required
 def updateProfile(request):
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=request.user)
