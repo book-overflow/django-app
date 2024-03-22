0829315e426c63d8a4673aa5971aee8e2fedf6c8
allan-jt <allan.thekkepeedika@gmail.com>
2024-03-22 14:16:03 -0400

    Automated superuser creation

--- src/django/init.sh
+++ src/django/init.sh
@@ -1,17 +1,19 @@
 cd /app
 
-echo 1
+echo 1. Installing
 
 pipenv install
 
-echo 2
+echo 2. Migrating
 
 pipenv run python manage.py makemigrations
 
-echo 3
-
 pipenv run python manage.py migrate
 
-echo 4
+echo 3. Creating superuser
+
+pipenv run python manage.py createsuperuser --noinput
+
+echo 4. Running server
 
 pipenv run python manage.py runserver 0.0.0.0:${BE_PORT}
