9caa13aa6e1ee9302ba838a4b46ec84596f332da
timothycao <tcao.pro@gmail.com>
2024-04-07 19:52:08 -0400

    Updated student_profile forms to fix saving issues

--- src/django/app/student_profile/views.py
+++ src/django/app/student_profile/views.py
@@ -5,12 +5,14 @@ from .forms import ProfileCreationForm, ProfileUpdateForm
 @login_required
 def createProfile(request):
     if request.method == 'POST':
-        form = ProfileCreationForm(request.POST, request.FILES, instance=request.user.student)
+        form = ProfileCreationForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            profile = form.save(commit=False)
+            profile.student = request.user.student
+            profile.save()
             return redirect('browse')
     else:
-        form = ProfileCreationForm(instance=request.user.student)
+        form = ProfileCreationForm()
     context = {'form': form}
     return render(request, 'registerProfile.html', context)
 
@@ -21,11 +23,11 @@ def getProfile(request):
 @login_required
 def updateProfile(request):
     if request.method == 'POST':
-        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.student)
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.student.profile)
         if form.is_valid():
             form.save()
             return redirect('user-profile')
     else:
-        form = ProfileUpdateForm(instance=request.user.student)
+        form = ProfileUpdateForm(instance=request.user.student.profile)
     context = {'form': form}
     return render(request, 'updateProfile.html', context)
