c0a3c766ab712cf38f7dfb9c684bfc680c03f10e
timothycao <tcao.pro@gmail.com>
2024-04-10 21:44:34 -0400

    Fixed resolution issue for activate url

--- src/django/app/user/urls.py
+++ src/django/app/user/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path("activate/<uidb64>/<token>", views.activate, name="activate"),
-    path("register/", views.register, name="register"),
-    path("login/", views.login, name="login"),
+    path("activate/<uidb64>/<token>", views.activate, name="activate")
 ]
