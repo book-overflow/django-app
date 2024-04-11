f7c46ee47069cff63c89759a63fef15392109644
timothycao <tcao.pro@gmail.com>
2024-04-10 22:00:22 -0400

    Added mail server configurations to settings.py

--- src/django/app/bookoverflow/settings.py
+++ src/django/app/bookoverflow/settings.py
@@ -111,6 +111,17 @@ DATABASES = {
 }
 
 
+# Mail server
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_FROM = os.environ.get('EMAIL_FROM')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = True
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
