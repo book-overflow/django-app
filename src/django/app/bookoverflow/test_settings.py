8b71f5b6bf2357202fd3506b53e0ad6ba43eeb7d
KryptonClarke <clarkepatrone@gmail.com>
2024-04-12 11:37:55 -0400

    trying another path

--- src/django/app/bookoverflow/test_settings.py
+++ src/django/app/bookoverflow/test_settings.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+
+
+Run this command to execute tests:
+python src/django/app/manage.py test --settings=bookoverflow.test_settings
 """
 
 from pathlib import Path
