224d7c43bddf7b35bb4b49c18663754dcaca4dcb
timothycao <tcao.pro@gmail.com>
2024-04-12 16:51:41 -0400

    Refactor TextbookCopyCreationForm: Course, Author, Textbook, TextbookCopy forms

--- src/django/app/textbook/forms.py
+++ src/django/app/textbook/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from shared.models import TextbookCopy
+from shared.models import Course, Author, Textbook, TextbookCopy
 
 class TextbookCopyCreationForm(ModelForm):
     isbn = forms.IntegerField()
@@ -8,6 +8,27 @@ class TextbookCopyCreationForm(ModelForm):
     authors = forms.CharField(max_length=255)
     course = forms.CharField(max_length=255)
     edition = forms.IntegerField()
+    class Meta:
+        model = TextbookCopy
+        fields = ['condition', 'for_rent', 'for_sale', 'sale_price', 'rent_price', 'image']
+
+class CourseCreationForm(ModelForm):
+    class Meta:
+        model = Course
+        fields = ['course_number', 'name', 'description']
+
+class AuthorCreationForm(ModelForm):
+    class Meta:
+        model = Author
+        fields = ['first_name', 'last_name']
+
+class TextbookCreationForm(ModelForm):
+    class Meta:
+        model = Textbook
+        fields = ['isbn', 'name', 'edition']
+
+class TextbookCopyCreationForm2(ModelForm):
+    textbook = TextbookCreationForm()
     class Meta:
         model = TextbookCopy
         fields = ['condition', 'for_rent', 'for_sale', 'sale_price', 'rent_price', 'image']
