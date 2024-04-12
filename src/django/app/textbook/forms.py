ca0ee388ba77fbaa2c9b492e585b9244c466bc89
timothycao <tcao.pro@gmail.com>
2024-04-12 17:53:23 -0400

    Refactor TextbookCopyCreationForm: Added Course and Author inline formset to Textbook form

--- src/django/app/textbook/forms.py
+++ src/django/app/textbook/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, inlineformset_factory
 from shared.models import Course, Author, Textbook, TextbookCopy
 
 class TextbookCopyCreationForm(ModelForm):
@@ -26,9 +26,19 @@ class TextbookCreationForm(ModelForm):
     class Meta:
         model = Textbook
         fields = ['isbn', 'name', 'edition']
+    
+    CourseFormSet = inlineformset_factory(Textbook, Course, form=CourseCreationForm, extra=0)
+    AuthorFormSet = inlineformset_factory(Textbook, Author, form=AuthorCreationForm, extra=0)
+    
+    def __init__(self, *args, **kwargs):
+        super(TextbookCreationForm, self).__init__(*args, **kwargs)
+        self.course_formset = self.CourseFormSet(*args, **kwargs)
+        self.author_formset = self.AuthorFormSet(*args, **kwargs)
+
 
 class TextbookCopyCreationForm2(ModelForm):
     textbook = TextbookCreationForm()
+    
     class Meta:
         model = TextbookCopy
         fields = ['condition', 'for_rent', 'for_sale', 'sale_price', 'rent_price', 'image']
